9f779af522b2996034bb994d24b1f1f366b4b24c
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-10-23 13:03:46 +0700

    Added a fade-in animation for the main content

--- app/src/main/java/nick/mirosh/newsapp/ui/feed/MainScreenContent.kt
+++ app/src/main/java/nick/mirosh/newsapp/ui/feed/MainScreenContent.kt
@@ -1,10 +1,19 @@
 package nick.mirosh.newsapp.ui.feed
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,13 +32,19 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,7 +74,7 @@ fun MainScreenContent(
             is FeedUIState.Loading -> /*LoadingProgressBar()*/SmileyAnimation()
             is FeedUIState.Failed -> FailedMessage()
             is FeedUIState.Feed ->
-                ArticleFeed(
+                ArticleFeed2(
                     articles = articles,
                     onArticleClick = onArticleClick,
                     onLikeClick = viewModel::onLikeClick,
@@ -103,6 +118,50 @@ fun ArticleFeed(
     )
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ArticleFeed2(
+    articles: List<Article>,
+    onArticleClick: (Article) -> Unit,
+    onLikeClick: (Article) -> Unit,
+    onSavedArticlesClicked: () -> Unit
+) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        isVisible = true
+    }
+
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(animationSpec = TweenSpec(durationMillis = 2000)),
+        exit = fadeOut(animationSpec = TweenSpec(durationMillis = 2000))
+    ) {
+        Scaffold(
+            content = {
+                LazyColumn {
+                    items(articles, key = { article -> article.url }) { article ->
+                        ArticleItem(article, onArticleClick, onLikeClick)
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = onSavedArticlesClicked,
+                    modifier = Modifier.padding(bottom = 16.dp, end = 16.dp)
+                ) {
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        imageVector = ImageVector.vectorResource(id = R.drawable.save),
+                        contentDescription = "Save"
+                    )
+                }
+            },
+            floatingActionButtonPosition = FabPosition.End,
+        )
+    }
+}
 @Composable
 fun ArticleItem(
     article: Article,
